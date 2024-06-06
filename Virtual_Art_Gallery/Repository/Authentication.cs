d49894e746ac31a4cb6cd746539758c61bdc3671
rohan_chougule <rohan_chougule@ROHAN>
2024-06-06 18:26:53 +0900

    Add project files.

---
+++ Virtual_Art_Gallery/Repository/Authentication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Virtual_Art_Gallery.Utility;
+using Virtual_Art_Gallery.Models;
+
+namespace Virtual_Art_Gallery.Repository
+{
+    internal class Authentication
+    {
+        SqlConnection connect = null;
+        SqlCommand cmd = null;
+        public Authentication()
+        {
+            connect = new SqlConnection(DataConnectionUtility.GetConnectionString());
+            cmd = new SqlCommand();
+        }
+
+        public bool Login(string username,string password)
+        {
+            int count = 0;
+            try
+            {
+                cmd.CommandText = "Select count(*) as match from [USER] where Username = @userName and Password = @pwd";
+                cmd.Parameters.AddWithValue("@userName", username);
+                cmd.Parameters.AddWithValue("@pwd", password);
+                connect.Open();
+                cmd.Connection = connect;
+                SqlDataReader reader = cmd.ExecuteReader();
+                cmd.Parameters.Clear();
+                while (reader.Read())
+                {
+                    count = (int)reader["match"];
+                }
+                connect.Close();
+                
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (count > 0)
+                return true;
+            return false;
+        }
+
+        public bool Register(User user)
+        {
+            int status = 0;
+            try
+            {
+                cmd.CommandText = "Insert into [USER] values (@username,@password,@email,@fname,@lname,@dob,@profile)";
+                cmd.Parameters.AddWithValue("@username", user.Username);
+                cmd.Parameters.AddWithValue("@password", user.Password);
+                cmd.Parameters.AddWithValue("@email", user.Email);
+                cmd.Parameters.AddWithValue("@fname", user.FirstName);
+                cmd.Parameters.AddWithValue("@lname", user.LastName);
+                cmd.Parameters.AddWithValue("@dob", user.DateOfBirth);
+                cmd.Parameters.AddWithValue("@profile", user.ProfilePicture);
+                connect.Open();
+                cmd.Connection = connect;
+                status = cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+                connect.Close();
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (status>0)
+                return true;
+            return false;
+        }
+    }
+}
